688c1bd131c170568df833321153f126aedf2dbc
Houda Maham <hudakpopiya@gmail.com>
2021-04-04 16:34:23 +0000

    second  commit just after writing my code

---
+++ main.js
@@ -0,0 +1,10 @@
+const Messages = [
+  "Have a nime day!",
+  "Be Happy",
+  " dont forget to improve your skills",
+  "Stay creative",
+  "Code , code and code ",
+  "Stay inspired ",
+  " try again",
+];
+console.log(Messages[Math.floor(Math.random * Messages.length)]);
